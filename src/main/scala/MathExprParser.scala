9c2565702d7140a3036ace55d5be4de3bb37e506
sulabhk <sulabh.kothari@gmail.com>
2018-09-04 11:31:37 +0530

    Refactor

--- src/main/scala/MathExprParser.scala
+++ src/main/scala/MathExprParser.scala
@@ -11,23 +11,25 @@ object Expr {
 case class Leaf(v: Float) extends Expr(None, None, None, Some(v))
 
 class MathExpr extends JavaTokenParsers {
-  def expr: Parser[Expr] = term ~ rep("+" ~ term | "-" ~ term) ^^ (moreParse(_))
+  def expr: Parser[Expr] = term ~ rep("+" ~ term | "-" ~ term) ^^ (buildParseTree(_))
 
-  def term: Parser[Expr] = factor ~ rep("*" ~ factor | "/" ~ factor) ^^ (moreParse(_))
+  def term: Parser[Expr] = factor ~ rep("*" ~ factor | "/" ~ factor) ^^ (buildParseTree(_))
 
   def factor: Parser[Expr] = floatingPointNumber ^^ (x => Leaf(x.toFloat)) | "(" ~> expr <~ ")"
 
-  def moreParse(p: Any): Expr = p match {
+  //Builds parse tree represented by Expr (left-op-right)
+  def buildParseTree(p: Any): Expr = p match {
     case x ~ List() if x.isInstanceOf[Expr] => x.asInstanceOf[Expr]
     case Leaf(x) ~ (y :: ys) =>
-      val v = getOpAndOperand(y, ys)
+      val v = resolveExprChain(y, ys)
       new Expr(Some(Leaf(x)), Some(v._1), Some(v._2), None)
     case x ~ (y :: ys) if x.isInstanceOf[Expr] =>
-      val v = getOpAndOperand(y, ys)
+      val v = resolveExprChain(y, ys)
       new Expr(Some(x.asInstanceOf[Expr]), Some(v._1), Some(v._2), None)
   }
 
-  def getOpAndOperand(y: Any, ys: Any): (String, Expr) = {
+  //Resolves repetitions represented by rep in Context free grammer
+  def resolveExprChain(y: Any, ys: Any): (String, Expr) = {
     val (m, n) = y match {
       case a ~ Leaf(b) => (a.toString, Leaf(b))
       case a ~ Expr(l, o, r, v) => (a.toString, new Expr(l, o, r, v))
@@ -35,7 +37,7 @@ class MathExpr extends JavaTokenParsers {
     if (ys.asInstanceOf[List[Any]].isEmpty) (m, n)
     else {
       val (p, q) = ys match {
-        case a :: b => getOpAndOperand(a, b)
+        case a :: b => resolveExprChain(a, b)
       }
       (m, new Expr(Some(n), Some(p), Some(q), None))
     }
@@ -57,10 +59,8 @@ object MathExprParser extends MathExpr {
 
   def main(args: Array[String]): Unit = {
     val exp = "((2+3)-4/(3*(((7+6)-4)/7)/2))"
-    println(parse(expr, exp))
     val result = parse(expr, exp)
-    println(result)
-    println(evaluate(result.get.asInstanceOf[Expr]).value)
+    println(evaluate(result.get).value)
     println(parseAll(expr, "((2+3)-4)/2"))
   }
 }

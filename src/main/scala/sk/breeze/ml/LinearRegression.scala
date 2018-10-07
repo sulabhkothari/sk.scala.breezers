81c0e8a32b65b69bfbd05bf1ff724f8b106cacc0
sulabhk <sulabh.kothari@gmail.com>
2018-10-08 00:00:31 +0530

    generic linear regression

--- src/main/scala/sk/breeze/ml/LinearRegression.scala
+++ src/main/scala/sk/breeze/ml/LinearRegression.scala
@@ -1,6 +1,6 @@
 package sk.breeze.ml
 
-import breeze.linalg.{*, DenseMatrix, DenseVector, linspace}
+import breeze.linalg.{*, DenseMatrix, DenseVector, inv, linspace, sum}
 import breeze.plot.{Figure, plot}
 import co.theasi.plotly.{Plot, ThreeDPlot, draw}
 
@@ -11,7 +11,50 @@ object LinearRegression {
   case class Plane(a: Double, b: Double, c: Double)
 
   def main(args: Array[String]): Unit = {
-    showSampleRegressionInXYZ
+    compareNormalEquationsAndGradientDescent
+  }
+
+  def compareNormalEquationsAndGradientDescent = {
+    val rg = new scala.util.Random
+    val denseMat = DenseMatrix.tabulate[Double](200, 2)((i, j) => if (j == 0) i + 1 else 2 + i + 30 * rg.nextDouble())
+
+    val X = denseMat(*, 0).underlying
+    val Y = denseMat(*, 1).underlying
+
+    val theta = normalEquations(denseMat)
+    val line = Line(theta(1), theta(0))
+    println(s"Normal Equation solution: (${line.c},${line.slope})")
+
+    val theta2 = gradientDescent(denseMat)
+
+    val line2 = Line(theta2(1), theta2(0))
+    val f = Figure()
+    val p = f.subplot(0)
+    p += plot(X, Y, '.')
+    println(s"Gradient Descent solution: (${line2.c},${line2.slope})")
+    val ls = linspace(0, X(X.length - 1))
+    p += plot(ls, ls :* line.slope :+ line.c)
+    p += plot(ls, ls :* line2.slope :+ line2.c, '+')
+
+  }
+
+  def showRegressionWithNormalEquations = {
+    val rg = new scala.util.Random
+    val denseMat = DenseMatrix.tabulate[Double](60, 2)((i, j) => if (j == 0) i + 1 else 2 + i + 30 * rg.nextDouble())
+
+    val X = denseMat(*, 0).underlying
+    val Y = denseMat(*, 1).underlying
+
+    val theta = normalEquations(denseMat)
+    val line = Line(theta(1), theta(0))
+    val f = Figure()
+    val p = f.subplot(0)
+    p += plot(X, Y, '.')
+    println(line.slope)
+    println(line.c)
+    val ls = linspace(0, X(X.length - 1))
+    println(ls)
+    p += plot(ls, ls :* line.slope :+ line.c)
   }
 
   def showSampleRegressionInXY = {
@@ -43,8 +86,8 @@ object LinearRegression {
     //    val X = denseMat(*, 0).underlying
     //    val Y = denseMat(*, 1).underlying
     //    val Z = denseMat(*, 2).underlying
-    val xs:Vector[Double] = (-8.0 to 8.0 by 0.1).toVector
-    val ys:Vector[Double] = (-8.0 to 8.0 by 0.1).toVector
+    val xs: Vector[Double] = (-8.0 to 8.0 by 0.1).toVector
+    val ys: Vector[Double] = (-8.0 to 8.0 by 0.1).toVector
     val zs = xs zip ys map {
       case (x, y) => x * rg.nextDouble() * 2000 - y * rg.nextDouble() * 1717
     }
@@ -77,9 +120,9 @@ object LinearRegression {
     var cTemp: Double = 0.0
 
     val count = X.length
-    val alpha: Double = 0.00005 / count
+    val alpha: Double = 0.005 / count
 
-    val precision = 0.000001 / count
+    val precision = 0.0001 / count
 
     while ((m - mTemp).abs > precision || (c - cTemp).abs > precision) {
       m = mTemp
@@ -96,9 +139,9 @@ object LinearRegression {
       mTemp = m - alpha * sumM / (2 * count)
       cTemp = c - alpha * sumC / (2 * count)
 
-      //      println(s"($sumM,$sumC)")
-      //      println(s"($mTemp,$cTemp)")
-      //      scala.io.StdIn.readLine()
+      //println(s"($sumM,$sumC)")
+      //println(s"($mTemp,$cTemp)")
+      //scala.io.StdIn.readLine()
     }
 
     Line(m, c)
@@ -137,9 +180,45 @@ object LinearRegression {
       println(s"($a,$b,$c)")
 
       println(s"($aTemp,$bTemp,$cTemp)")
-      //scala.io.StdIn.readLine()
+      scala.io.StdIn.readLine()
     }
 
     Plane(a, b, c)
+  }
+
+  def isConverged(theta: DenseVector[Double], thetaTemp: DenseVector[Double], precision: Double) = {
+    (theta - thetaTemp).data.map(_.abs).forall(_ < precision)
+  }
+
+  def gradientDescent(data: DenseMatrix[Double]): DenseVector[Double] = {
+    var theta = DenseVector.ones[Double](data.cols)
+    var thetaTemp = DenseVector.zeros[Double](data.cols)
+    val y = data(*, data.cols - 1).underlying
+    val features = prependOnesColumn(data(*, 0 to data.cols - 2).underlying)
+    val count:Double = data.rows
+    val alpha: Double = 0.05 / count
+    val precision = 0.000001 // count
+
+    while (!isConverged(theta, thetaTemp, precision)) {
+      theta = thetaTemp
+      val diff = (features * theta - y)
+      val result = diff.t * features
+      thetaTemp = theta - (result.t :* alpha :/ (2 * count))
+    }
+    thetaTemp
+  }
+
+  def normalEquations(data: DenseMatrix[Double]) = {
+    val featuresWithoutOnes = data(*, 0 to data.cols - 2).underlying
+    val features = prependOnesColumn(featuresWithoutOnes)
+    val y = data(*, data.cols - 1).underlying
+    val theta = inv(features.t * features) * features.t * y
+    theta
+  }
+
+  def prependOnesColumn(original: DenseMatrix[Double]): DenseMatrix[Double] = {
+    val ones = DenseVector.ones[Double](original.rows)
+    val dataWithOnes = ones.data ++ original.data
+    DenseMatrix.create(original.rows, original.cols + 1, dataWithOnes)
   }
 }
